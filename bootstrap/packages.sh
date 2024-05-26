2889cd797eae0fe6de9ae0e301caf721516be126
Vai <vai.sriv@icloud.com>
2024-05-26 15:47:35 -0700

    Update packages.sh

--- bootstrap/packages.sh
+++ bootstrap/packages.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 if [ grep -q "Arch Linux" /etc/os-release ] 2>/dev/null; then
 	echo "archlinux detected"
-	yay -Syu --needed <packages.arch.txt
+	paru -Syu --needed <packages.arch.txt
 else
 	echo "this system is not archlinux, and is unsupported. please proceed with caution"
 	# No operations
