7c2322296bb185fe27477cc524eb5779aa795a86
Vai <vai.sriv@icloud.com>
2024-11-08 12:13:40 -0500

    long overdue update to xdg and fish with custom dir locations

--- user/config/fish/conf.d/01-env.fish
+++ user/config/fish/conf.d/01-env.fish
@@ -8,4 +8,16 @@ set -x GPG_TTY $(tty)
 
 # user env vars
 set -x FILES_DIR $HOME/files
+set -x DOWNLOADS_DIR $FILES_DIR/downloads
+set -x PICTURES_DIR $FILES_DIR/pictures
 set -x NOTES_DIR $FILES_DIR/notes
+
+# xdg
+set XDG_DESKTOP_DIR $HOME/Desktop
+set XDG_DOCUMENTS_DIR $HOME/Documents
+set XDG_DOWNLOAD_DIR $DOWNLOADS_DIR
+set XDG_MUSIC_DIR $HOME/Music
+set XDG_PICTURES_DIR $PICTURES_DIR
+set XDG_PUBLICSHARE_DIR $HOME/Public
+set XDG_TEMPLATES_DIR $HOME/Templates
+set XDG_VIDEOS_DIR $HOME/Videos
