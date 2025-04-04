b4a9bd63173ed7764d4eb8086517375598d043fa
vai <vai.sriv@icloud.com>
2025-04-04 13:17:26 -0400

    setup dock lol

--- darwin/optional/desktop-env/common/programs/pkgs.nix
+++ darwin/optional/desktop-env/common/programs/pkgs.nix
@@ -5,11 +5,6 @@
 	lib,
 	...
 }: {
-	# gui apps
 	environment.systemPackages = with pkgs; [
-		bitwarden
-		qview
-		# TODO: find an audio app and can stream spotify hopefully lol
-		vscodium
 	];
 }
