3299122d719abc62a13fdb8b8a0bf9c78b8f7a33
vai <vai.sriv@icloud.com>
2025-04-07 12:24:37 -0400

    configure many nix-darwin settings

--- darwin/optional/desktop-env/common/system/bar.nix
+++ darwin/optional/desktop-env/common/system/bar.nix
@@ -5,7 +5,28 @@
 	lib,
 	...
 }: {
-	system.defaults = {
-		menuExtraClock.Show24Hour = true; # show 24 hour clock
+	system = {
+		defaults = {
+			menuExtraClock = {
+				Show24Hour = true;
+				ShowDayOfMonth = true;
+				ShowDayOfWeek = true;
+				ShowSeconds = true;
+			};
+
+			NSGlobalDomain = {
+				AppleICUForce24HourTime = true;
+			};
+
+			controlcenter = {
+				AirDrop = false;
+				BatteryShowPercentage = true;
+				Bluetooth = false;
+				Display = false;
+				FocusModes = false;
+				NowPlaying = false;
+				Sound = false;
+			};
+		};
 	};
 }
