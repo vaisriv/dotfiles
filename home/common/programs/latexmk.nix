c9318441f770a80eadd407bf4ca7492e13e1e951
vai <vai.sriv@icloud.com>
2025-04-07 13:03:09 -0400

    temp move latexmkrc to home-manager (rn using file creation, eventually will make a module)

---
+++ home/common/programs/latexmk.nix
@@ -0,0 +1,36 @@
+{
+	config,
+	pkgs,
+	inputs,
+	lib,
+	...
+}: {
+	# TODO: actually implement latexmk and its rc in home-manager
+	# programs.latexmk = {
+	# 	enable = true;
+	# 	package = pkgs.texlivePackage.latexmk;
+	# 	settings = {
+	# 		aux_dir = "./.texaux/";
+	# 		aux_out_dir_report = 1;
+	# 		bibtex_use = 2;
+	# 		out_dir = "./";
+	# 		pdf_mode = 1;
+	# 		show_time = 1;
+	# 		silent = 1;
+	# 	};
+	# };
+	home.file = {
+		".config/latexmk/latexmkrc" = {
+			text = ''
+				$aux_dir = "./.texaux/";
+				$aux_out_dir_report = 1;
+				$bibtex_use = 2;
+				$out_dir = "./";
+				$pdf_mode = 1;
+				$show_time = 1;
+				$silent = 1;
+			'';
+			executable = false;
+		};
+	};
+}
