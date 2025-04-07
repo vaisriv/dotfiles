a2dda31811cbafcc11db7cbf8e19303255282ebf
vai <vai.sriv@icloud.com>
2025-04-07 17:42:10 -0400

    add jdks

--- darwin/optional/uni/programs/pkgs.nix
+++ darwin/optional/uni/programs/pkgs.nix
@@ -8,5 +8,14 @@
 	environment.systemPackages = with pkgs; [
 		texliveFull
 		zk
+		jdk8
+		jdk11
+		jdk23
 	];
+
+	system.activationScripts.extraActivation.text = ''
+		ln -sf "${pkgs.jdk8}/zulu-8.jdk" "/Library/Java/JavaVirtualMachines/"
+		ln -sf "${pkgs.jdk11}/zulu-11.jdk" "/Library/Java/JavaVirtualMachines/"
+		ln -sf "${pkgs.jdk23}/zulu-23.jdk" "/Library/Java/JavaVirtualMachines/"
+	'';
 }
