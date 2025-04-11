49cb4d11579b26ce46c3ad283a3e8fe009bf0158
vai <vai.sriv@icloud.com>
2025-04-11 13:01:35 -0400

    add qutebrowser

---
+++ home/optional/desktop-env/common/programs/qutebrowser.nix
@@ -0,0 +1,27 @@
+{
+	config,
+	pkgs,
+	inputs,
+	lib,
+	...
+}: {
+	programs.qutebrowser = {
+		enable = true;
+		settings = {
+			url = {
+				auto_search = true;
+			};
+			/* tabs = {
+				tabs_are_windows = false;
+			}; */
+		};
+		searchEngines = {
+			DEFAULT = "https://unduck.astrophel.xyz/?q={}&default_bang=ddg";
+			ddg = "https://www.duckduckgo.com/search?hl=en&amp;q={}";
+			g = "https://www.google.com/search?hl=en&amp;q={}";
+			w = "https://en.wikipedia.org/wiki/Special:Search?search={}&amp;go=Go&amp;ns0=1";
+			aw = "https://wiki.archlinux.org/?search={}";
+			nw = "https://wiki.nixos.org/index.php?search={}";
+		};
+	};
+}
