bdf66e3d1332a59f32ee401e7438b0988d5ea7c4
vai <vai.sriv@icloud.com>
2025-05-02 13:42:39 -0400

    add tasksh and minimal config for taskwarrior

--- home/common/programs/taskwarrior.nix
+++ home/common/programs/taskwarrior.nix
@@ -9,16 +9,22 @@
 		taskwarrior = {
 			enable = true;
 			package = pkgs.taskwarrior3;
+			config = {
+				journal.time = "on";
+				search.case.sensitive = "no";
+			};
 		};
 	};
 
 	# services = {
 	# 	taskwarrior-sync = {
 	# 		enable = true;
+	#               frequency = "*:0/5";
 	# 	};
 	# };
 
 	home.packages = with pkgs; [
 		taskwarrior-tui
+		tasksh
 	];
 }
