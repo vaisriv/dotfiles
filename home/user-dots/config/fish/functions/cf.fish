302bc497a5b6d9638f598ecb025b690cef92be1a
vai <vai.sriv@icloud.com>
2025-05-05 12:00:54 -0400

    create nvim + git-diff function

--- home/user-dots/config/fish/functions/cf.fish
+++ home/user-dots/config/fish/functions/cf.fish
@@ -2,10 +2,10 @@
 function cf
 	set root_dir $argv[1]
 	if test -z "$root_dir"
-	set root_dir "."
+		set root_dir "."
 	end
 	set file (fzf --walker-root=$root_dir)
 	if test -n "$file"
-	    cat "$file"
+		cat "$file"
 	end
 end
