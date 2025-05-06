c8316369aef3c797fcdc651c797510d7fd8b786b
vai <vai.sriv@icloud.com>
2025-05-06 10:26:36 -0400

    add matlab to path

--- home/user-dots/config/fish/conf.d/03-uni.fish
+++ home/user-dots/config/fish/conf.d/03-uni.fish
@@ -4,6 +4,9 @@ set -x UNI_NOTES_DIR $UNI_DIR/notes
 set -x HW_TEX_TEMPLATE $UNI_DIR/msc/tex/HWTemplate.tex
 #set -x --path TEXINPUT . $UNI_DIR/msc/tex/ $TEXINPUTS
 
+fish_add_path /Applications/MATLAB_R2024b.app/bin
+alias jl="julia --project=. -i"
+
 # needed for marp-cli
 # set -x CHROME_PATH $(which chromium)
 
