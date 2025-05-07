1da5eca759cd7b86c92e5c7cded6b01f87e25f09
vai <vai.sriv@icloud.com>
2025-05-06 21:39:49 -0400

    add `jls` function for running homework code files

--- home/user-dots/config/fish/conf.d/03-uni.fish
+++ home/user-dots/config/fish/conf.d/03-uni.fish
@@ -6,6 +6,12 @@ set -x HW_TEX_TEMPLATE $UNI_DIR/msc/tex/HWTemplate.tex
 
 fish_add_path /Applications/MATLAB_R2024b.app/bin
 alias jl="julia --project=. -i"
+function jls
+	set jlfile (string join '' "./code/" $argv[1] ".jl")
+	set outfile (string join '' "./code/" $argv[1] ".txt")
+	echo "\$ julia --project=. $jlfile $argv[2..] | tee $outfile"
+	julia --project=. $jlfile $argv[2..] | tee $outfile
+end
 
 # needed for marp-cli
 # set -x CHROME_PATH $(which chromium)
