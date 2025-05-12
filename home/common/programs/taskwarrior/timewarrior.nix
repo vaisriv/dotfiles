cdfc4690168fbe57bba4f8af58050d61d83bcd49
vai <vai.sriv@icloud.com>
2025-05-12 14:07:51 -0400

    fix timewarrior path

--- home/common/programs/taskwarrior/timewarrior.nix
+++ home/common/programs/taskwarrior/timewarrior.nix
@@ -13,7 +13,7 @@
 		timewarrior-hook = {
 			enable = true;
 			executable = true;
-			target = "~/.local/share/task/hooks/on-modify.timewarrior";
+			target = "$HOME/.local/share/task/hooks/on-modify.timewarrior";
 			source = "${pkgs.timewarrior}/share/doc/timew/ext/on-modify.timewarrior";
 		};
 	};
