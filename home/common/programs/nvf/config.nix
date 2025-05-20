8baa19bade33254a8fda1414338ba2b13ea63629
vai <vai.sriv@icloud.com>
2025-05-20 15:13:56 -0400

    added some stuff to nvf but still disabled and nowhere near done

--- home/common/programs/nvf/config.nix
+++ home/common/programs/nvf/config.nix
@@ -9,61 +9,15 @@
 		settings = {
 			vim = {
 				viAlias = false;
-				vimAlias = true;
+				vimAlias = false;
 				enableLuaLoader = true;
-				autocomplete = {
-					enableSharedCmpSources = true;
-					blink-cmp = {
-						enable = true;
-						friendly-snippets.enable = true;
-					};
-				};
-				spellcheck = {
-					enable = true;
-					ignoredFiletypes = [
-						"gitignore"
-						"gitcommit"
-					];
-					languages = [ "en" ];
-					programmingWordlist.enable = true;
-				};
-				lsp = {
-					enable = true;
-					formatOnSave = true;
-					trouble.enable = true;
-					nvim-docs-view.enable = true;
-				};
-				languages = {
-					enableFormat = true;
-					enableTreesitter = true;
-
-					# languages
-					clang = {
-						enable = true;
-						cHeader = false;
-					};
-					bash.enable = true;
-					html.enable = true;
-					julia.enable = false; # unfortunate but wont build on darwin
-					lua.enable = true;
-					markdown = {
-						enable = true;
-						extensions.render-markdown-nvim.enable = true;
-					};
-					nix.enable = true;
-					python.enable = true;
-					r.enable = true;
-					rust = {
-						enable = true;
-						crates.enable = true;
-					};
-					ts = {
-						enable = true;
-						extensions.ts-error-translator.enable = true;
-					};
-					yaml.enable = true;
-				};
 			};
 		};
+	};
+
+	stylix.targets.nvf = {
+		enable = true;
+		plugin = "mini-base16";
+		transparentBackground = false;
 	};
 }
