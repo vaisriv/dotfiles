6e4274534d4724810c23c5a32990022036aea1d9
vai <vai.sriv@icloud.com>
2025-05-28 12:59:53 -0700

    fix postactivation on darwin

--- darwin/common/system/configuration.nix
+++ darwin/common/system/configuration.nix
@@ -7,10 +7,10 @@
 }: {
 	system = {
 		# activationScripts are executed every time you boot the system or run `nixos-rebuild` / `darwin-rebuild`.
-		activationScripts.text = ''
+		activationScripts.postActivation.text = ''
 			# activateSettings -u will reload the settings from the database and apply them to the current session,
 			# so we do not need to logout and login again to make the changes take effect.
-			/System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
+			sudo -u /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
 		'';
 	};
 
