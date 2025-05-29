c5b33754449803491cddbbee6ffe48651fb21e04
vai <vai.sriv@icloud.com>
2025-05-29 00:30:26 -0700

    idk shit mpd conf that does not work

--- home/common/programs/ncmpcpp.nix
+++ home/common/programs/ncmpcpp.nix
@@ -9,11 +9,38 @@
 		ncmpcpp = {
 			enable = true;
 			# bindings = {};
-			mpdMusicDir = "$XDG_MUSIC_DIR/library";
+			mpdMusicDir = "$files/music";
 			settings = {
 				ncmpcpp_directory = "~/.local/share/ncmpcpp";
 				mpd_host = "localhost";
 				mpd_port = 6600;
+			};
+		};
+	};
+	services = {
+		mpd = {
+			enable = true;
+			musicDirectory = "$files/music";
+			extraConfig = ''
+				playlist_directory "~/.config/mpd/playlists"
+
+				db_file "~/.config/mpd/mpd.db"
+				log_file "~/.config/mpd/mpd.log"
+				pid_file "~/.config/mpd/mpd.pid"
+				state_file "~/.config/mpd/state"
+				sticker_file "~/.config/mpd/sticker.sql"
+
+				auto_update "yes"
+
+				audio_output {
+					type "osx"
+					name "CoreAudio"
+					mixer_type "software"
+				}
+			'';
+			network = {
+				listenAddress = "localhost";
+				port = 6600;
 			};
 		};
 	};
