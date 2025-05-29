7c7fbd1e21a4e5ac8274669d2041a1fbf7d78e2a
vai <vai.sriv@icloud.com>
2025-05-29 00:38:50 -0700

    yay mpd works yay

--- home/common/programs/ncmpcpp.nix
+++ home/common/programs/ncmpcpp.nix
@@ -17,30 +17,30 @@
 			};
 		};
 	};
-	services = {
-		mpd = {
-			enable = true;
-			musicDirectory = "$files/music";
-			extraConfig = ''
-				playlist_directory "~/.config/mpd/playlists"
+	home = {
+		file = {
+			mpd-conf = {
+				enable = true;
 
-				db_file "~/.config/mpd/mpd.db"
-				log_file "~/.config/mpd/mpd.log"
-				pid_file "~/.config/mpd/mpd.pid"
-				state_file "~/.config/mpd/state"
-				sticker_file "~/.config/mpd/sticker.sql"
+				target = ".config/mpd/mpd.conf";
+				text = ''
+					music_directory "~/media/music"
+					playlist_directory "~/.config/mpd/playlists"
+					db_file "~/.config/mpd/mpd.db"
+					log_file "~/.config/mpd/mpd.log"
+					pid_file "~/.config/mpd/mpd.pid"
+					state_file "~/.config/mpd/state"
+					sticker_file "~/.config/mpd/sticker.sql"
+					bind_to_address "localhost"
+					port "6600"
+					auto_update "yes"
 
-				auto_update "yes"
-
-				audio_output {
-					type "osx"
-					name "CoreAudio"
-					mixer_type "software"
-				}
-			'';
-			network = {
-				listenAddress = "localhost";
-				port = 6600;
+					audio_output {
+						type "osx"
+						name "CoreAudio"
+						mixer_type "software"
+					}
+				'';
 			};
 		};
 	};
