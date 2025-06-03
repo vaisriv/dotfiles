633f7058fd92f7a52a8a08ad6b955ca9814205cf
vai <vai.sriv@icloud.com>
2025-06-03 13:09:37 -0700

    fix yazi shell wrapper in fish

--- home/optional/shell/common/functions.nix
+++ home/optional/shell/common/functions.nix
@@ -67,6 +67,17 @@
 				'';
 				description = "weather in terminal!";
 			};
+			yy = {
+				body = ''
+					set -l tmp (mktemp -t "yazi-cwd.XXXXX")
+					command yazi $argv --cwd-file="$tmp"
+					if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+						builtin cd -- "$cwd"
+					end
+					command rm -f -- "$tmp"
+				'';
+				description = "yazi shell wrapper (change cwd using yazi)";
+			};
 		};
 	};
 }
