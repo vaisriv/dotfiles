7192074f372675149d962cf029c93deb480091f4
vai <vai.sriv@icloud.com>
2025-06-09 20:52:41 -0700

    swap certain apps back to homebrew

--- darwin/optional/desktop-env/aerospace/programs/pkgs.nix
+++ darwin/optional/desktop-env/aerospace/programs/pkgs.nix
@@ -7,7 +7,6 @@
 }: {
   environment.systemPackages = with pkgs; [
     alt-tab-macos # window switcher
-    itsycal # calendar/date bar widget
     qview # image viewer
     raycast # launcher
   ];
