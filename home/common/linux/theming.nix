5898ad1c9c8b21cde82440d7732ba657be2b5410
vai <vai.sriv@icloud.com>
2025-06-11 16:38:00 -0700

    add treefmt via toml

--- home/common/linux/theming.nix
+++ home/common/linux/theming.nix
@@ -10,16 +10,16 @@
 in {
   stylix = {
     cursor = {
-    	name = "catppuccin-${flavor}-${accent}-cursors";
-    	package = pkgs.catppuccin-cursors.${flavor + lib.toUpper (lib.substring 0 1 accent) + (lib.substring 1 (lib.stringLength accent) accent)};
-    	size = 24;
+      name = "catppuccin-${flavor}-${accent}-cursors";
+      package = pkgs.catppuccin-cursors.${flavor + lib.toUpper (lib.substring 0 1 accent) + (lib.substring 1 (lib.stringLength accent) accent)};
+      size = 24;
     };
 
     iconTheme = {
-    	enable = true;
-    	dark = "Papirus-Dark";
-    	light = "Papirus-Light";
-    	package = pkgs.catppuccin-papirus-folders.override {inherit accent flavor;};
+      enable = true;
+      dark = "Papirus-Dark";
+      light = "Papirus-Light";
+      package = pkgs.catppuccin-papirus-folders.override {inherit accent flavor;};
     };
   };
 }
