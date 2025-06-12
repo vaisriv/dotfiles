e1b48396858cfc9f39ae4bd51ab08ce949e2512a
vai <vai.sriv@icloud.com>
2025-06-12 00:18:56 -0700

    fix jvm location linking

--- darwin/optional/uni/programs/pkgs.nix
+++ darwin/optional/uni/programs/pkgs.nix
@@ -13,9 +13,9 @@
         jdk23
     ];
 
-    # system.activationScripts.postActivation.text = ''
-    #   ln -sf ${pkgs.jdk8}/zulu-8.jdk /Library/Java/JavaVirtualMachines/
-    #   ln -sf ${pkgs.jdk11}/zulu-11.jdk /Library/Java/JavaVirtualMachines/
-    #   ln -sf ${pkgs.jdk23}/zulu-23.jdk /Library/Java/JavaVirtualMachines/
-    # '';
+    system.activationScripts.postActivation.text = ''
+      ln -sf ${pkgs.jdk8}/Library/Java/JavaVirtualMachines/zulu-8.jdk /Library/Java/JavaVirtualMachines/zulu-8.jdk
+      ln -sf ${pkgs.jdk11}/Library/Java/JavaVirtualMachines/zulu-11.jdk /Library/Java/JavaVirtualMachines/zulu-11.jdk
+      ln -sf ${pkgs.jdk23}/Library/Java/JavaVirtualMachines/zulu-23.jdk /Library/Java/JavaVirtualMachines/zulu-23.jdk
+    '';
 }
