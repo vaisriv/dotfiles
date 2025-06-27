0e8fc56cd9e3ba251f14dea5439b4297fbe0a35c
vai <vai.sriv@icloud.com>
2025-06-26 20:00:40 -0700

    add quarto

--- home/user-dots/config/nvim/lua/plugins/r.lua
+++ home/user-dots/config/nvim/lua/plugins/r.lua
@@ -1,4 +1,16 @@
 return {
+	{
+		"quarto-dev/quarto-nvim",
+		dependencies = {
+			"jmbuhr/otter.nvim",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		config = function()
+			local quarto = require("quarto")
+			quarto.setup()
+			vim.keymap.set("n", "<leader>mp", quarto.quartoPreview, { silent = true, noremap = true })
+		end,
+	},
 	--[[ {
 		"R-nvim/R.nvim",
 		ft = { "r", "rmd", "qmd" },
