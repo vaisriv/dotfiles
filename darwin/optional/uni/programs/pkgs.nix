0873a2e8034f84e35df11189c327fdb35ff885c5
vai <vai.sriv@icloud.com>
2025-07-02 15:06:36 -0700

    format

--- darwin/optional/uni/programs/pkgs.nix
+++ darwin/optional/uni/programs/pkgs.nix
@@ -16,8 +16,8 @@
     ];
 
     system.activationScripts.postActivation.text = ''
-      ln -sf ${pkgs.jdk8}/Library/Java/JavaVirtualMachines/zulu-8.jdk /Library/Java/JavaVirtualMachines/zulu-8.jdk
-      ln -sf ${pkgs.jdk11}/Library/Java/JavaVirtualMachines/zulu-11.jdk /Library/Java/JavaVirtualMachines/zulu-11.jdk
-      ln -sf ${pkgs.jdk23}/Library/Java/JavaVirtualMachines/zulu-23.jdk /Library/Java/JavaVirtualMachines/zulu-23.jdk
+        ln -sf ${pkgs.jdk8}/Library/Java/JavaVirtualMachines/zulu-8.jdk /Library/Java/JavaVirtualMachines/zulu-8.jdk
+        ln -sf ${pkgs.jdk11}/Library/Java/JavaVirtualMachines/zulu-11.jdk /Library/Java/JavaVirtualMachines/zulu-11.jdk
+        ln -sf ${pkgs.jdk23}/Library/Java/JavaVirtualMachines/zulu-23.jdk /Library/Java/JavaVirtualMachines/zulu-23.jdk
     '';
 }
