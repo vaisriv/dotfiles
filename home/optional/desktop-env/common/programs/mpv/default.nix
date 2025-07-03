d2199a35dd6ed0088b5a2bac199c003d21e5100e
vai <vai.sriv@icloud.com>
2025-07-02 19:03:08 -0700

    mpv config working, and mostly nix-ified

--- home/optional/desktop-env/common/programs/mpv/default.nix
+++ home/optional/desktop-env/common/programs/mpv/default.nix
@@ -7,23 +7,20 @@
 }: {
     programs.mpv.enable = true;
 
-    # imports = [
-    #     ./config.nix
-    #     ./bindings.nix
-    #     ./scripts.nix
-    # ];
+    imports = [
+        ./config.nix
+        ./bindings.nix
+        ./scripts.nix
+    ];
 
     home = {
-        packages = with pkgs; [
-            python3Packages.guessit
-        ];
         file = {
-            mpv-config = {
-                enable = false;
+            mpv-fonts = {
+                enable = true;
                 recursive = true;
 
-                target = ".config/mpv";
-                source = ../../../../../user-dots/config/mpv;
+                target = ".config/mpv/fonts";
+                source = ./fonts;
             };
         };
     };
