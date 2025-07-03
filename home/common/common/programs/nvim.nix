9bc98090652000e431579e67111b81790331af41
vai <vai.sriv@icloud.com>
2025-07-02 17:13:20 -0700

    add personal nvf config

--- home/common/common/programs/nvim.nix
+++ home/common/common/programs/nvim.nix
@@ -5,43 +5,9 @@
     lib,
     ...
 }: {
-    # TODO: finish nvf config
-    # programs.nvim = {
-    #   enable = true;
-    #   package = inputs.nvf.packages.${pkgs.system}.default;
-    # };
-
     home = {
         packages = with pkgs; [
-            # nvim
-            neovim
-
-            # lsp
-            ccls
-            gopls
-            lua-language-server
-            matlab-language-server
-            nil
-            rust-analyzer
-            rustc
-            texlab
-            tree-sitter
-
-            # formatters
-            stylua
-
-            # langs
-            gcc
-            nodejs
+            inputs.nvf-config.packages.${pkgs.system}.lua
         ];
-        file = {
-            nvim-config = {
-                enable = true;
-                recursive = true;
-
-                target = ".config/nvim";
-                source = ../../../user-dots/config/nvim;
-            };
-        };
     };
 }
