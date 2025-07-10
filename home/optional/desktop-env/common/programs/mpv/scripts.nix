8814ee219fa60bcf589298f2efec718b08378f68
vai <vai.sriv@icloud.com>
2025-07-10 13:10:51 -0700

    add (disabled) ani-skip mpv script

--- home/optional/desktop-env/common/programs/mpv/scripts.nix
+++ home/optional/desktop-env/common/programs/mpv/scripts.nix
@@ -12,12 +12,53 @@
         sponsorblock-minimal
     ];
 
-    # autochapters mpv script (https://github.com/po5/mpv-auto-chapters)
     home = {
         packages = with pkgs; [
+            # ani-skip
             python3Packages.guessit
         ];
         file = {
+            # ani-skip mpv script (https://github.com/synacktraa/ani-skip)
+            ani-skip = {
+                enable = false;
+
+                target = ".config/mpv/scripts/ani-skip/skip.lua";
+                text =
+                    # lua
+                    ''
+                        -- Importing mpv module
+                        local mpv = require('mp')
+                        local mpv_options = require("mp.options")
+
+                        local options = { -- setting default options
+                            op_start = 0, op_end = 0, ed_start = 0, ed_end = 0,
+                        }
+                        mpv_options.read_options(options, "skip") --reading script-opts data
+
+                        -- Main function to check and skip if within the defined section
+                        local function skip()
+                            local current_time = mp.get_property_number("time-pos")
+
+                            if not current_time then
+                                return
+                            end
+
+                            -- Check for opening sequence
+                            if current_time >= options.op_start and current_time < options.op_end then
+                                mp.set_property_number("time-pos", options.op_end)
+                            end
+
+                            -- Check for ending sequence
+                            if current_time >= options.ed_start and current_time < options.ed_end then
+                                mp.set_property_number("time-pos", options.ed_end)
+                            end
+                        end
+
+                        -- Bind the function to be called whenever the time position is changed
+                        mp.observe_property("time-pos", "number", skip)
+                    '';
+            };
+            # autochapters mpv script (https://github.com/po5/mpv-auto-chapters)
             autochapters = {
                 enable = true;
 
