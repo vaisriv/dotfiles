70e80341b71a5d1b0c029a8a77a05a53dee16ac7
vai <vai.sriv@icloud.com>
2025-07-14 13:57:16 -0700

    switch to bleeding-edge lix

--- flake.nix
+++ flake.nix
@@ -32,13 +32,13 @@
 
         ## Better Nix Implementation - Lix
         lix = {
-            url = "https://git.lix.systems/lix-project/lix/archive/2.91.1.tar.gz";
+            url = "https://git.lix.systems/lix-project/lix/archive/main.tar.gz";
             flake = false;
         };
 
         ## Lix Module
         lix-module = {
-            url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.1.tar.gz";
+            url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz";
             inputs = {
                 nixpkgs.follows = "nixpkgs";
                 lix.follows = "lix";
@@ -78,7 +78,7 @@
                 config.allowUnfree = true;
                 overlays = [
                     # inputs.self.overlays.default
-                    inputs.lix-module.overlays.lixFromNixpkgs
+                    inputs.lix-module.overlays.default
                     inputs.nixos-apple-silicon.overlays.default
                 ];
             };
