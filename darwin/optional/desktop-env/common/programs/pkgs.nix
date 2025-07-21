a0ebbb08b0a9c69373a33145cf469b29fcd5631e
vai <vai.sriv@icloud.com>
2025-07-21 13:16:52 -0400

    add musescore

--- darwin/optional/desktop-env/common/programs/pkgs.nix
+++ darwin/optional/desktop-env/common/programs/pkgs.nix
@@ -6,12 +6,15 @@
     ...
 }: {
     environment.systemPackages = with pkgs; [
+        # recommended
         discord # chat app
         iina # media/video player
         spotify # music
         vscodium # backup text/code editor
 
+        # optional
         appcleaner # used to remove/clean manually-installed apps
+        musescore # music transcription tool
         qbittorrent # bitorrent
     ];
 }
