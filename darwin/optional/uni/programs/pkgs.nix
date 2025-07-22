afa40fe4b8e9d30c9aecc3c3b247bd0ff9f0d16e
vai <vai.sriv@icloud.com>
2025-07-22 15:32:33 -0400

    fix darwin-rebuild complaining about overwriting symlinks to java libs

--- darwin/optional/uni/programs/pkgs.nix
+++ darwin/optional/uni/programs/pkgs.nix
@@ -16,6 +16,8 @@
     ];
 
     system.activationScripts.postActivation.text = ''
+        mkdir -p /Library/Java/JavaVirtualMachines/
+        find /Library/Java/JavaVirtualMachines/ -type l -name "*.jdk" -delete
         ln -sf ${pkgs.jdk8}/Library/Java/JavaVirtualMachines/zulu-8.jdk /Library/Java/JavaVirtualMachines/zulu-8.jdk
         ln -sf ${pkgs.jdk11}/Library/Java/JavaVirtualMachines/zulu-11.jdk /Library/Java/JavaVirtualMachines/zulu-11.jdk
         ln -sf ${pkgs.jdk23}/Library/Java/JavaVirtualMachines/zulu-23.jdk /Library/Java/JavaVirtualMachines/zulu-23.jdk
