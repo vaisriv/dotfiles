07d966e12d4e5dd331dba93331e9635f52298b75
vai <vai.sriv@icloud.com>
2025-07-22 15:42:36 -0400

    disable atuin up-arrow bind

--- home/common/common/programs/atuin.nix
+++ home/common/common/programs/atuin.nix
@@ -9,8 +9,12 @@
         enable = true;
         daemon.enable = true;
 
+        flags = [
+            "--disable-up-arrow"
+        ];
+
         settings = {
-            enter_accept = false;
+            # enter_accept = false;
             style = "compact";
             dotfiles.enabled = false;
         };
