0b4af024c7cfd21b4b4e4c49f08aede0a2ecb398
vai <vai.sriv@icloud.com>
2025-07-23 18:48:19 -0400

    add custom icons for nix-related dirs to eza

--- home/common/common/programs/eza.nix
+++ home/common/common/programs/eza.nix
@@ -15,5 +15,25 @@
             "--group-directories-first"
             "--header"
         ];
+
+        # custom icons for nix & direnv dirs
+        theme = {
+            filenames = {
+                "nix".icon.glyph = "";
+                ".nix".icon.glyph = "";
+                ".direnv".icon.glyph = "";
+            };
+        };
+    };
+
+    # putting the config file in `~/.config/eza` (as done by programs.eza.theme) is broken on macos
+    # (https://github.com/eza-community/eza/issues/1224)
+    home.file = {
+        eza-theme = {
+            enable = pkgs.stdenv.isDarwin;
+
+            target = "Library/Application Support/eza/theme.yml";
+            source = (pkgs.formats.yaml {}).generate "eza-theme" config.programs.eza.theme;
+        };
     };
 }
