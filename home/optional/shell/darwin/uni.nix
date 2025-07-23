f29a669e08043bd51f53af14b0ec0c46bb799f64
vai <vai.sriv@icloud.com>
2025-07-23 15:00:25 -0400

    remove julia from homebrew (now handled by nix yay)

--- home/optional/shell/darwin/uni.nix
+++ home/optional/shell/darwin/uni.nix
@@ -10,7 +10,6 @@
             # add useful uni apps to path
             "/Applications/Zen.app/Contents/MacOS" # uni browser
             "/Applications/MATLAB_R2024b.app/bin" # matlab gui/lang
-            "/Applications/Julia-1.11.app/Contents/Resources/julia/bin" # julia lang
         ];
         sessionVariables = {
             # needed for marp-cli
