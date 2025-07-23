134f84d6e0b844a397e87030cfdfc7d1ffef4920
vai <vai.sriv@icloud.com>
2025-07-23 15:00:25 -0400

    remove julia from homebrew (now handled by nix yay)

--- darwin/optional/uni/programs/homebrew.nix
+++ darwin/optional/uni/programs/homebrew.nix
@@ -19,8 +19,6 @@
 
             # WARN: currently broken on nix :)
             "mathpix-snipping-tool"
-            # julia is broken on nix due to a lack of fhs env. want to eventually be handled by nix need to figure out shell.nix/flake.nix fhs-env or similar to get packages/libraries to actually work and not have linking errors
-            "julia-app"
 
             # NOTE: installed manually:
             # umd vpn - palo-alto globalprotect-openconnect # umd's custom build of palo-alt vpn
