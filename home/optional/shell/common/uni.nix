8e99c25c9430fb918847541ef58dffb1b3adda17
vai <vai.sriv@icloud.com>
2025-07-28 15:10:50 -0400

    move uni julia functions and alias to uni devshell

--- home/optional/shell/common/uni.nix
+++ home/optional/shell/common/uni.nix
@@ -18,40 +18,6 @@
         shellAliases = {
             # allow uni shell script to cd user
             uni = "source $UNI_DIR/script";
-
-            # julia helper
-            jl = "julia --project=. -i";
-        };
-        functions = {
-            jl = {
-                description = "run julia script and drop into interactive shell for given problem using local project environment";
-                body =
-                    # fish
-                    ''
-                        set jlfile (string join \'\' "./code/" $prob".jl")
-                        echo "\$ julia --project=. -i $jlfile $args"
-                        julia --project=. -i $jlfile $args
-                    '';
-                argumentNames = [
-                    "prob"
-                    "args"
-                ];
-            };
-            jls = {
-                description = "run and log output of julia script for given problem using local project environment";
-                body =
-                    # fish
-                    ''
-                        set jlfile (string join \'\' "./code/" $prob ".jl")
-                        set outfile (string join \'\' "./code/" $prob ".txt")
-                        echo "\$ julia --project=. $jlfile $args | tee $outfile"
-                        julia --project=. $jlfile $args | tee $outfile
-                    '';
-                argumentNames = [
-                    "prob"
-                    "args"
-                ];
-            };
         };
     };
 }
