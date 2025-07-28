a72e02dc9ca2b693bf2c0fca6b57e7a6b6b754dc
vai <vai.sriv@icloud.com>
2025-07-28 14:43:08 -0400

    update uni_dir shell vars

--- home/optional/shell/common/uni.nix
+++ home/optional/shell/common/uni.nix
@@ -10,8 +10,8 @@
         ];
         sessionVariables = {
             # uni dirs
-            UNI_DIR = "$FILES_DIR/uni";
-            UNI_NOTES_DIR = "$UNI_DIR/notes";
+            UNI_DIR = "$HOME/files/uni";
+            UNI_NOTES_DIR = "$HOME/files/uni/notes";
         };
     };
     programs.fish = {
