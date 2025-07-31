a90f8b5b7ad3901545d8610283f0f17361661f22
vai <vai.sriv@icloud.com>
2025-07-31 15:21:28 -0400

    swap option and command for all non-apple devices in karabiner-elements

---
+++ home/optional/desktop-env/darwin/wms/aerospace/services/karabiner-elements/default.nix
@@ -0,0 +1,16 @@
+{
+    config,
+    pkgs,
+    inputs,
+    lib,
+    ...
+}: {
+    home.file = {
+        karabiner-elements-config = {
+            enable = true;
+
+            target = ".config/karabiner/karabiner.json";
+            source = ./karabiner.json;
+        };
+    };
+}
