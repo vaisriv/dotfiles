6ba3fba4975b6508f64f276061ab7502031e6a47
vai <vai.sriv@icloud.com>
2025-07-31 15:05:10 -0400

    remove shell and add treefmt-nix config

---
+++ nix/formatter.nix
@@ -0,0 +1,39 @@
+{
+    pkgs,
+    inputs,
+    ...
+}:
+inputs.treefmt-nix.lib.mkWrapper pkgs {
+    projectRootFile = "flake.nix";
+
+    # nix
+    programs = {
+        deadnix.enable = true;
+        alejandra.enable = true;
+    };
+    settings.formatter = {
+        alejandra = {
+            options = [
+                "--experimental-config"
+                "./.nix/fmt/alejandra.toml"
+            ];
+        };
+    };
+
+    # toml
+    programs.prettier = {
+        enable = true;
+        settings = {
+            tabWidth = 4;
+        };
+    };
+
+    # bash
+    programs.shfmt = {
+        enable = true;
+        indent_size = 4;
+    };
+
+    # fish
+    programs.fish_indent.enable = true;
+}
--- nix/shell/default.nix
+++
@@ -1,5 +0,0 @@
-# Shell for bootstrapping flake-enabled nix and home-manager
-# You can enter it through 'nix develop' or (legacy) 'nix-shell'
-{pkgs ? import <nixpkgs> {}}: {
-    default = import ./shell.nix {inherit pkgs;};
-}
