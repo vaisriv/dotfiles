35cbded7f8ab0f82a39e7082f2d7615e87941981
vai <vai.sriv@icloud.com>
2025-09-17 17:25:06 -0400

    feat(home/shell): update uni app env paths

--- home/optional/shell/darwin/uni.nix
+++ home/optional/shell/darwin/uni.nix
@@ -1,9 +1,9 @@
-{...}: {
+{username, ...}: {
     home = {
         sessionPath = [
             # add useful uni apps to path
-            "/Applications/Zen.app/Contents/MacOS" # uni browser
-            "/Applications/MATLAB_R2024b.app/bin" # matlab gui/lang
+            "${pkgs.nur.repos.natsukium.zen-browser}/Applications/Zen.app/Contents/MacOS" # uni browser
+            "/Users/${username}/Applications/Uni/MATLAB_R2024b.app/bin" # matlab gui/lang
         ];
         sessionVariables = {
             # needed for marp-cli
