2958e788c9e19560070666478636593aa9ebe65b
vai <vai.sriv@icloud.com>
2025-09-30 14:40:05 -0400

    fix(darwin/aerospace): move raycast from nixpkgs to homebrew

--- darwin/optional/desktop-env/aerospace/programs/homebrew.nix
+++ darwin/optional/desktop-env/aerospace/programs/homebrew.nix
@@ -6,14 +6,15 @@
         casks = [
             # TODO: package and upstream to nixpkgs
             "batfi" # battery management widget
+            "jdownloader" # download manager (alt)
             "jordanbaird-ice" # bar management
             "linearmouse" # per-mouse settings
             "motrix" # download manager
-            "jdownloader" # download manager (alt)
 
-            # WARN: currently broken on nix :)
+            # WARN: currently broken/out-of-date on nix :)
             "itsycal" # calendar/date bar widget
             "qview" # image viewer
+            "raycast" # launcher
 
             # NOTE: installed manually
             # "daisydisk"
--- darwin/optional/desktop-env/aerospace/programs/pkgs.nix
+++ darwin/optional/desktop-env/aerospace/programs/pkgs.nix
@@ -1,6 +1,5 @@
 {pkgs, ...}: {
     environment.systemPackages = with pkgs; [
         alt-tab-macos # window switcher
-        raycast # launcher
     ];
 }
