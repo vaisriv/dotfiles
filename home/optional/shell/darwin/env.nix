3d956f5cb72ba0b62e214fc4e8d1d7095ad0cd27
vai <vai.sriv@icloud.com>
2025-10-08 12:13:51 -0400

    feat(home/shell): add velja browser picker alias and env var

--- home/optional/shell/darwin/env.nix
+++ home/optional/shell/darwin/env.nix
@@ -5,15 +5,20 @@
             "$HOME/.local/bin"
 
             # add useful apps to path
-            "/Applications/Firefox.app/Contents/MacOS" # browser
-            "/Applications/qutebrowser.app/Contents/MacOS" # alt browser
             "/Applications/VSCodium.app/Contents/MacOS" # backup text/code editor
             "/Applications/IINA.app/Contents/MacOS" # media player
             "/Applications/mpv.app/Contents/MacOS" # media player
             "/Applications/qView.app/Contents/MacOS" # image viewer
         ];
         sessionVariables = {
-            # BROWSER = "velja";
+            BROWSER = "velja";
+        };
+    };
+
+    programs.fish = {
+        shellAliases = {
+            # velja browser picker
+            velja = "open -a Velja";
         };
     };
 }
