fed2b1793722f82c80994798af0121a56c092a62
vai <vai.sriv@icloud.com>
2025-10-08 13:02:46 -0400

    feat(darwin/common): set user's shell automatically

--- darwin/common/users.nix
+++ darwin/common/users.nix
@@ -1,11 +1,18 @@
 {
     username,
     fullname,
+    pkgs,
     ...
 }: {
-    users.users."${username}" = {
-        home = "/Users/${username}";
-        description = "${fullname}";
+    users = {
+        users."${username}" = {
+            home = "/Users/${username}";
+            description = "${fullname}";
+
+            uid = 501;
+            shell = pkgs.fish;
+        };
+        knownUsers = [ "${username}" ];
     };
     system.primaryUser = "${username}";
 }
