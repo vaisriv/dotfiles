91c02331fdbf5d22b9aa68301b58cc1442d4840a
vai <vai.sriv@icloud.com>
2025-11-11 12:01:33 -0500

    feat(home/lazygit): create config at proper path for darwin

--- home/common/common/programs/lazygit.nix
+++ home/common/common/programs/lazygit.nix
@@ -1,4 +1,9 @@
-{...}: {
+{
+    pkgs,
+    lib,
+    config,
+    ...
+}: {
     programs.lazygit = {
         enable = true;
         settings = {
@@ -10,5 +15,10 @@
                 overrideGpg = true;
             };
         };
+    };
+
+    xdg.configFile."lazygit/config.yml" = lib.mkIf pkgs.stdenv.isDarwin {
+        # source = config.lib.file.mkOutOfStoreSymlink "Library/Application Support/lazygit/config.yml";
+        source = (pkgs.formats.yaml {}).generate "lazygit-config" config.programs.lazygit.settings;
     };
 }
