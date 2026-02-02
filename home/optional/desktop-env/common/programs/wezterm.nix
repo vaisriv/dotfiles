6f185c943f9b9622a723f47d4cff77844769d1cc
Vai Srivastava <vaisriv@icloud.com>
2026-02-02 13:47:28 -0500

    feat(home/wezterm): add keybind for claude-code

--- home/optional/desktop-env/common/programs/wezterm.nix
+++ home/optional/desktop-env/common/programs/wezterm.nix
@@ -4,13 +4,15 @@
         extraConfig =
             # lua
             ''
-                local wezterm = require 'wezterm'
                 local act = wezterm.action
                 local config = wezterm.config_builder()
 
                 config = {
                     hide_tab_bar_if_only_one_tab = true,
                     font_size = 16,
+                    keys = {
+                        {key="Enter", mods="SHIFT", action=wezterm.action{SendString="\x1b\r"}},
+                    }
                 }
 
                 return config
