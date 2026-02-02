260f6d5a3e6ac6aad64dde7eec01b6bef66e8ef4
Vai Srivastava <vaisriv@icloud.com>
2026-02-02 16:12:32 -0500

    feat(home/wezterm): add more customization

--- home/optional/desktop-env/common/programs/wezterm.nix
+++ home/optional/desktop-env/common/programs/wezterm.nix
@@ -8,10 +8,21 @@
                 local config = wezterm.config_builder()
 
                 config = {
+                    -- title + tab bar
+                    use_fancy_tab_bar = false,
                     hide_tab_bar_if_only_one_tab = true,
+
+                    -- font configs
                     font_size = 16,
+                    adjust_window_size_when_changing_font_size = false,
+
+                    -- keybinds
                     keys = {
-                        {key="Enter", mods="SHIFT", action=wezterm.action{SendString="\x1b\r"}},
+                        {
+                            key="Enter",
+                            mods="SHIFT",
+                            action=wezterm.action{SendString="\x1b\r"}
+                        },
                     }
                 }
 
