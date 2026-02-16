50b8a178158b267042a82fcc09e5ece0c6bb7911
Vai Srivastava <vaisriv@icloud.com>
2026-02-16 14:52:31 -0500

    feat(home/shell): update fish nf function to allow initial query via argument

--- home/optional/shell/common/functions/nf.fish
+++ home/optional/shell/common/functions/nf.fish
@@ -1,14 +1,21 @@
 #!/usr/bin/env fish
 
-set -l root_dir $argv[1]
-if test -z "$root_dir"
-    set root_dir "."
+# set -l root_dir $argv[1]
+# if test -z "$root_dir"
+#     set root_dir "."
+# end
+
+set -l search_term $argv[1]
+if test -z "$search_term"
+    set search_term ""
 end
 
-set -l file (fzf --walker-root="$root_dir")
+# set -l file (fzf --walker-root="$root_dir")
+set -l file (fzf -q "$search_term")
 if test -n "$file"
     # nvim "$file"
     set -l escaped (string escape -- $file)
     commandline -r -- "nvim $escaped"
     commandline -f repaint
 end
+
