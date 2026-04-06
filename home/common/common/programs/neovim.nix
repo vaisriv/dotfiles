19613d5f7d4c8b7f01488c2aa5dd0ae1645c5166
Vai Srivastava <vaisriv@icloud.com>
2026-04-06 19:40:31 +0000

    feat(home/neovim): swap from "minimal" to "vai" config

--- home/common/common/programs/neovim.nix
+++ home/common/common/programs/neovim.nix
@@ -6,8 +6,7 @@
 {
     home = {
         packages = with pkgs; [
-            # inputs.nvf-config.packages.${pkgs.stdenv.hostPlatform.system}.minimal
-            inputs.nvf-config.packages.${pkgs.stdenv.hostPlatform.system}.lua
+            inputs.nvf-config.packages.${pkgs.stdenv.hostPlatform.system}.vai
         ];
     };
 }
