e52bdf68c4c6209fcc2ea994dbad3566d58be6df
Vai Srivastava <vaisriv@icloud.com>
2026-09-24 15:31:01 -0400

    feat(home/nix-conf): add nixpkgs-unstable to flake registry

--- home/common/common/nix-conf.nix
+++ home/common/common/nix-conf.nix
@@ -1,4 +1,4 @@
-{ ... }: {
+{ inputs, ... }: {
     nix = {
         # Set up Nix garbage collector
         gc = {
@@ -9,6 +9,21 @@
 
         # Auto reuse pre-existing binaries/paths in nix-store
         settings.auto-optimise-store = true;
+
+        # Set nix registries (for use with `nix run/shell/etc <flake>#<program>`)
+        registry = {
+            nixpkgs-unstable = {
+                from = {
+                    id = "nixpkgs-unstable";
+                    type = "indirect";
+                };
+                to = {
+                    path = inputs.nixpkgs-unstable.outPath;
+                    type = "path";
+                };
+            };
+        };
+
     };
 
     imports = [
